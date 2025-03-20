c2fc68a4ba0461c898ed3d866debe77191c67b6a
jekakoba <endlesss2k2k@gmail.com>
2025-03-20 17:38:01 +0200

    category part 3

---
+++ .history/src/js/custom_20250320170333.js
@@ -0,0 +1,70 @@
+function fullSearchInput() {
+	document.addEventListener("click", function (e) {
+		const targetElement = e.target;
+		if (targetElement.closest("#search")) {
+			document.documentElement.classList.add("search-open");
+			e.preventDefault();
+		} else if (targetElement.closest("[data-close-search]")) {
+			document.documentElement.classList.remove("search-open");
+		}
+	});
+}
+
+function searchFormClear() {
+	const inputSearch = document.querySelector("input[name='search']"),
+		clearButton = document.querySelector("#search-form-clear");
+	if (!inputSearch && clearButton) return;
+
+	inputSearch.addEventListener("input", function () {
+		if (inputSearch.value.trim()) {
+			clearButton.hidden = false;
+		} else {
+			clearButton.hidden = true;
+		}
+	});
+	clearButton.addEventListener("click", function (e) {
+		inputSearch.value = "";
+		clearButton.hidden = true;
+	});
+}
+function updatePlaceholder() {
+	const inputsPlaceholder = document.querySelectorAll('[data-switch-placeholder]');
+	if (!inputsPlaceholder.length) return;
+	inputsPlaceholder.forEach(input => {
+		const mobilePlaceholder = input.getAttribute('data-mobile-placeholder');
+		if (window.matchMedia("(max-width: 767px)").matches) {
+			input.placeholder = mobilePlaceholder;
+		} else {
+			input.placeholder = "Search for products...";
+		}
+	});
+}
+function showFilter() {
+	const filter = document.querySelector('[data-filter]');
+	const filterButton = document.querySelector('[data-show-filter]');
+	if (!filter || !filterButton) return;
+
+	filterButton.addEventListener('click', (e) => {
+		document.documentElement.classList.toggle('show-filter');
+		document.documentElement.classList.toggle('lock');
+	});
+
+	function closeFilter() {
+		document.addEventListener('click', (e) => {
+			const clickedInsideFilter = !e.target.closest('[data-filter]');
+			const clickedOnButton = e.target.closest('[data-show-filter]');
+			if (clickedInsideFilter && !clickedOnButton) {
+				document.documentElement.classList.remove('show-filter');
+				document.documentElement.classList.remove('lock');
+			}
+		});
+	}
+	closeFilter();
+}
+document.addEventListener("DOMContentLoaded", function (e) {
+	fullSearchInput();
+	searchFormClear()
+	updatePlaceholder();
+	showFilter()
+});
+window.addEventListener("resize", updatePlaceholder);
---
+++ .history/src/js/libs/nouislider_20250320114312.js
@@ -0,0 +1,41 @@
+import noUiSlider from 'nouislider';
+import 'nouislider/dist/nouislider.css';
+let ranges = document.querySelectorAll('[data-nouislider]');
+if (ranges.length > 0) {
+	ranges.forEach(range => {
+		// We read the settings from Data Attributes
+		const start = JSON.parse(range.dataset.start || '[0, 100]'); // The initial values ​​of the handles
+		const rangeConfig = JSON.parse(range.dataset.range || '{"min": 0, "max": 10000}'); // Range
+		const step = parseInt(range.dataset.step || '1'); // Step
+		const margin = parseInt(range.dataset.margin || '10'); // The minimum distance between handles
+		const connect = range.dataset.connect === 'true'; // The connecting strip
+		const orientation = range.dataset.orientation || 'horizontal'; // Orientation
+		const direction = range.dataset.direction || 'ltr'; // Direction
+		const tooltips = range.dataset.tooltips === 'true'; // Tips
+		const pips = range.dataset.pips === 'true'; // Pips
+		let calcMargin = rangeConfig.max * 0.2
+
+		// Ініціалізація слайдера
+		noUiSlider.create(range, {
+			range: rangeConfig,
+			step: step,
+			start: start,
+			// margin: margin,
+			margin: calcMargin,
+			connect: connect,
+			orientation: orientation,
+			direction: direction,
+			tooltips: tooltips,
+			pips: pips,
+			// Formatting values ​​(leave in javaScript since it's logic)
+			format: {
+				to: function (value) {
+					return '$' + Math.floor(value); // Format: $ 50, $ 200
+				},
+				from: function (value) {
+					return Number(value.replace('$', '')); // For reverse conversion
+				}
+			}
+		});
+	});
+}
